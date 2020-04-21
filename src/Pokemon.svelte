6746abaa919eec4033bce0fbf61cabb4ce3a65d6
WarrenShekyls <warrenshekyls@gmail.com>
2020-04-21 09:52:11 +0100

    styling updates

--- src/Pokemon.svelte
+++ src/Pokemon.svelte
@@ -34,6 +34,15 @@
     width: 9rem;
     height: auto;
   }
+
+  div.pokemon div.body {
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    border-top: 1px solid gainsboro;
+
+  }
 </style>
 
 {#if pokeData}
@@ -41,13 +50,13 @@
     <div class="header">
       <h1>{poke.name.toUpperCase()}</h1>
       <PokemonTypes types={pokeData.types.sort((a,b) => (a.slot > b.slot) ? 1 : ((b.slot > a.slot) ? -1 : 0))} />
+    </div>
+    <div class="body">
+      <!-- component for stats -->
       <img
         class="pokemon-image"
         src="https://pokeres.bastionbot.org/images/pokemon/{pokeData.id}.png"
         alt="{poke.name.toUpperCase()} image" />
-    </div>
-    <div class="body">
-      <!-- componenet for stats -->
     </div>
   </div>
 {:else}
